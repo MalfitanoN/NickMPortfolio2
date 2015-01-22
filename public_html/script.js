6ae577d9f61a68ff3e49a483879266ec5b03329f
Nicholas Malfitano <nicholas1malfitano@gmail.com>
2015-01-21 23:19:37 -0800

    new jqurey things

---
+++ public_html/script.js
@@ -0,0 +1,69 @@
+$("document").ready(function(){
+    $(".Main").css("background-color", "blue");
+    
+    $("div:nth-child(1)").css("border-style", "solid");
+    
+    $("h1").bind("mouseover", mouseOverMe).bind("mouseout", mouseOutMe);
+    
+    $("h1").bind("click", mouseClick);
+    
+    $("#replaceWText").bind("click", replaceWText);
+    
+    $("#randPara").bind("click", addAPara);
+    
+    $("#removePara").bind("click", removeAPara);
+    
+    $("#tabs").tabs();
+    
+    $("#accordion").draggable();
+    
+    $("#tabs").draggable();
+    
+    $("#show").css("visibility", "hidden");
+    
+    $("#hide").bind("click", hideThePage);
+    
+    $("#show").bind("click", showThePage);
+    
+    $( "#accordion" ).accordion({
+      event: "click hoverintent"
+    });
+    
+ });
+ 
+ function removeAPara(){
+      $("#replaceWText p:last").remove();
+ } 
+ 
+ function addAPara(){
+      $("#replaceWText").append("<p>GET LEDLED</p>");
+ } 
+ 
+ function replaceWText(){
+      $("#replaceWText").text("Wrecked");
+ } 
+ 
+ function mouseOverMe(){
+     $("h1").html("LEDL");
+     $(".Main").css("background-color", "aqua");
+ } 
+ 
+ function mouseOutMe(){
+     $("h1").html("WOW");
+     $(".Main").css("background-color", "blue");
+ }
+ 
+ function mouseClick(){
+     $("p").html("YEA");
+ }
+ 
+ function hideThePage(){
+     $("#show").css("visibility", "visible");
+     $("div").hide("fade", {}, 2500);
+     $("#show").show("fade", {}, 2500);
+ }
+ 
+ function showThePage(){
+     $("div").show("fade", {}, 2500);
+     $("#show").hide("fade", {}, 2500);
+ }
